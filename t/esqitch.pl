282e1ea466289c5e37327f105aff5540717a0a49
Brian McKeen <brian.mckeen@nhs.net>
2015-03-24 11:16:21 +0000

    Re-order sqitch add arguments again

--- t/esqitch.pl
+++ t/esqitch.pl
@@ -8,13 +8,15 @@ if ($ARGV[0] eq 'add')
 {
     $addarg[0] = 'add';
     $addarg[1] = $ARGV[1] . '.' . $ARGV[2] . '.' . $ARGV[3]; # Name of Object (type.schema.name)
-    $addarg[2] = '--template-directory ' . $ARGV[6] . '/sqitch/etc/templates/'; # $ARGV[6] is the Eclipse workspace location
+    $addarg[2] = '--template-directory ' . $ARGV[5] . '/sqitch/etc/templates/'; # $ARGV[5] is the Eclipse workspace location
     $addarg[3] = '-t sqlcmd.' . $ARGV[1]; # $ARGV[1] is the template type (table,view,schema etc.)
-    $addarg[4] = '-r ' . join(' -r ',split(/,/,$ARGV[4])); # $ARGV[4] is a comma-separated list of dependencies
-    $addarg[5] = '-s object_schema=' . $ARGV[2]; # $ARGV[2] is the schema for the object (used in template)
-    $addarg[6] = '-s object_name=' . $ARGV[3]; # $ARGV[3] is the object name (used in template)
-    $addarg[7] = '-n "' . $ARGV[5] .'"'; # $ARGV[5] is the Git commit message
-
+    $addarg[4] = '-s object_schema=' . $ARGV[2]; # $ARGV[2] is the schema for the object (used in template)
+    $addarg[5] = '-s object_name=' . $ARGV[3]; # $ARGV[3] is the object name (used in template)
+    $addarg[6] = '-n "' . $ARGV[4] .'"'; # $ARGV[4] is the Git commit message
+    if ($ARGV[6] ne '')
+    {
+    	$addarg[7] = '-r ' . join(' -r ',split(/,/,$ARGV[6])); # $ARGV[6] is a comma-separated list of dependencies
+    }
     system($^X, $dirname . "\\sqitch.pl", @addarg);
     exit;
 }
