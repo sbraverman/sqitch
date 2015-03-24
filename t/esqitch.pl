c240e9d2d47a13e3bf341ff7197abd4113e09980
Brian McKeen <brian.mckeen@nhs.net>
2015-03-24 09:28:36 +0000

    Refactor sqitch calling files

---
+++ t/esqitch.pl
@@ -0,0 +1,44 @@
+use IPC::System::Simple qw(system capture);
+use File::Basename;
+
+my $dirname = dirname(__FILE__);
+my @addarg = [];
+
+if ($ARGV[0] eq 'add') 
+{
+    $addarg[0] = 'add';
+    $addarg[1] = $ARGV[4] . '.' . $ARGV[5] . '.' . $ARGV[6]; # Name of Object (type.schema.name)
+    $addarg[2] = '--template-directory ' . $ARGV[1] . '/sqitch/etc/templates/'; # $ARGV[1] is the Eclipse workspace location
+    $addarg[3] = '-t sqlcmd.' . $ARGV[4]; # $ARGV[4] is the template type (table,view,schema etc.)
+    $addarg[4] = '-r ' . join(' -r ',split(/,/,$ARGV[3])); # $ARGV[3] is a comma-separated list of dependencies
+    $addarg[5] = '-s object_schema=' . $ARGV[5]; # $ARGV[5] is the schema for the object (used in template)
+    $addarg[6] = '-s object_name=' . $ARGV[6]; # $ARGV[6] is the object name (used in template)
+    $addarg[7] = '-n "' . $ARGV[7] .'"'; # $ARGV[7] is the Git commit message
+
+    system($^X, $dirname . "\\sqitch.pl", @addarg);
+    exit;
+}
+
+if ($ARGV[0] eq 'help')
+{
+	system($^X, $dirname . "\\sqitch.pl", @ARGV);
+	exit;
+} 
+
+if ($ARGV[0] eq '' && $ARGV[1] eq '')
+{
+    system($^X, $dirname . "\\sqitch.pl", 'help');
+    exit;
+} 
+
+if (($ARGV[1] eq 'alpha' || $ARGV[1] eq 'beta' || $ARGV[1] eq 'live') && 
+    ($ARGV[0] eq 'checkout' || $ARGV[0] eq 'deploy' || $ARGV[0] eq 'rebase' || $ARGV[0] eq 'revert' || $ARGV[0] eq 'verify'))
+    {
+    	system("dsmod group \"CN=Informatics Systems Admins, OU=Informatics Service Accounts, OU=Service Accounts, DC=sdhc, DC=xsdhis, DC=nhs, DC=uk\" -addmbr \"CN=%USERNAME%, OU=Informatics Team, OU=Unmanaged Users, OU=Users, OU=SDHIS, DC=sdhc, DC=xsdhis, DC=nhs, DC=uk\"");
+    	system($^X, $dirname . "\\sqitch.pl", @ARGV);
+    	system("dsmod group \"CN=Informatics Systems Admins, OU=Informatics Service Accounts, OU=Service Accounts, DC=sdhc, DC=xsdhis, DC=nhs, DC=uk\" -rmmbr \"CN=%USERNAME%, OU=Informatics Team, OU=Unmanaged Users, OU=Users, OU=SDHIS, DC=sdhc, DC=xsdhis, DC=nhs, DC=uk\"");
+    }
+    else
+    {
+    	system($^X, $dirname . "\\sqitch.pl", @ARGV);
+    }
--- t/sqitch.pl
+++ t/sqitch.pl
@@ -1,44 +1,20 @@
-use IPC::System::Simple qw(system capture);
-use File::Basename;
+#perl -w
 
-my $dirname = dirname(__FILE__);
-my @ecladdout = [];
+# SJ 2015-02-10: fter perl 5.10 -CA is invalid on #! line so replace with these use statements
+use open ':std', ':utf8';
+use open IO => ':bytes';
 
-if ($ARGV[0] eq 'ecladd') # ecladd indicates that the Sqitch Add was called from Eclipse
-{
-    $ecladdout[0] = 'add';
-    $ecladdout[1] = $ARGV[4] . '.' . $ARGV[5] . '.' . $ARGV[6]; # Name of Object (type.schema.name)
-    $ecladdout[2] = '--template-directory ' . $ARGV[1] . '/sqitch/etc/templates/'; # $ARGV[1] is the Eclipse workspace location
-    $ecladdout[3] = '-t sqlcmd.' . $ARGV[4]; # $ARGV[4] is the template type (table,view,schema etc.)
-    $ecladdout[4] = '-r ' . join(' -r ',split(/,/,$ARGV[3])); # $ARGV[3] is a comma-separated list of dependencies
-    $ecladdout[5] = '-s object_schema=' . $ARGV[5]; # $ARGV[5] is the schema for the object (used in template)
-    $ecladdout[6] = '-s object_name=' . $ARGV[6]; # $ARGV[6] is the object name (used in template)
-    $ecladdout[7] = '-n "' . $ARGV[7] .'"'; # $ARGV[7] is the Git commit message
-
-    system($^X, $dirname . "\\run_sqitch.pl", @ecladdout);
-    exit;
+use POSIX qw(setlocale);
+BEGIN {
+    if ($^O eq 'MSWin32') {
+        require Win32::Locale;
+        setlocale POSIX::LC_ALL, Win32::Locale::get_locale();
+    } else {
+        setlocale POSIX::LC_ALL, '';
+    }
 }
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+use App::Sqitch;
 
-if ($ARGV[0] eq 'help')
-{
-	system($^X, $dirname . "\\run_sqitch.pl", @ARGV);
-	exit;
-} 
-
-if ($ARGV[0] eq '' && $ARGV[1] eq '')
-{
-    system($^X, $dirname . "\\run_sqitch.pl", 'help');
-    exit;
-} 
-
-if (($ARGV[1] eq 'alpha' || $ARGV[1] eq 'beta' || $ARGV[1] eq 'live') && 
-    ($ARGV[0] eq 'checkout' || $ARGV[0] eq 'deploy' || $ARGV[0] eq 'rebase' || $ARGV[0] eq 'revert' || $ARGV[0] eq 'verify'))
-    {
-    	system("dsmod group \"CN=Informatics Systems Admins, OU=Informatics Service Accounts, OU=Service Accounts, DC=sdhc, DC=xsdhis, DC=nhs, DC=uk\" -addmbr \"CN=%USERNAME%, OU=Informatics Team, OU=Unmanaged Users, OU=Users, OU=SDHIS, DC=sdhc, DC=xsdhis, DC=nhs, DC=uk\"");
-    	system($^X, $dirname . "\\run_sqitch.pl", @ARGV);
-    	system("dsmod group \"CN=Informatics Systems Admins, OU=Informatics Service Accounts, OU=Service Accounts, DC=sdhc, DC=xsdhis, DC=nhs, DC=uk\" -rmmbr \"CN=%USERNAME%, OU=Informatics Team, OU=Unmanaged Users, OU=Users, OU=SDHIS, DC=sdhc, DC=xsdhis, DC=nhs, DC=uk\"");
-    }
-    else
-    {
-    	system($^X, $dirname . "\\run_sqitch.pl", @ARGV);
-    }
+exit App::Sqitch->go;
